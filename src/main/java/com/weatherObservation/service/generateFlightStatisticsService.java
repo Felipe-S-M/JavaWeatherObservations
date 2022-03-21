eee2e5f5897a5c40e4842e3fd1454f36b253905a
Felipe <felipeschutz2025@hotmail.com>
2022-03-21 10:34:55 -0300

    statistics refactoring

--- src/main/java/com/weatherObservation/service/generateFlightStatisticsService.java
+++ src/main/java/com/weatherObservation/service/generateFlightStatisticsService.java
@@ -1,5 +1,6 @@
 package com.weatherObservation.service;
 
+import com.google.common.util.concurrent.AtomicDouble;
 import com.weatherObservation.FlightStatisticsMapper;
 import com.weatherObservation.dto.response.GenerateFlightStatisticsResponse;
 import org.springframework.stereotype.Service;
@@ -19,37 +20,15 @@ public class generateFlightStatisticsService {
         if(!FileUtilsService.checkIfExistFile("flyingData")){
             throw new Exception("Data file not found!");
         }
-
         Map<String, Double> resultsTemperature = new HashMap<>();
         Map<String, Integer> observationsEachObservatory = new HashMap<>();
-        Double totalDistance = 0.0;
-        String latPoints = "";
+        AtomicDouble totalDistance = 0.0;
+        StringBuilder latPoints = new StringBuilder();
 
         List<String> fileLines = getFileData();
         for (String line: fileLines) {
             String[] content = line.split("\\|");
-            if (!content[0].equals("id")) {
-                observationsEachObservatory.put(content[4], observationsEachObservatory.get(content[4]) + 1);
-
-                if (!latPoints.isEmpty()) {
-                    totalDistance = calculateTotalDistance(latPoints, content[2], totalDistance);
-                }
-                latPoints = content[2];
-
-                Double temperature = getConvertedTemperature(new Double(content[3]), content[4]);
-                if (resultsTemperature.get("minTemperature") == null ||
-                        temperature < resultsTemperature.get("minTemperature")) {
-                    resultsTemperature.put("minTemperature", temperature);
-                }
-                if (resultsTemperature.get("maxTemperature") == null ||
-                        temperature > resultsTemperature.get("maxTemperature")) {
-                    resultsTemperature.put("maxTemperature", temperature);
-                }
-                resultsTemperature.put("totalTemperature",
-                        resultsTemperature.get("totalTemperature") == null ? temperature :
-                                resultsTemperature.get("totalTemperature") + temperature);
-
-            }
+            buildObservationData(content);
         }
 
         return FlightStatisticsMapper.buildFlightStatistics(
@@ -58,6 +37,29 @@ public class generateFlightStatisticsService {
                 resultsTemperature.get("meanTemperature") / fileLines.size() - 1,
                 totalDistance,
                 observationsEachObservatory);
+    }
+
+    private void buildObservationData(String[] content) {
+        if (!content[0].equals("id")) {
+            observationsEachObservatory.put(content[4], observationsEachObservatory.get(content[4]) + 1);
+            calculateTotalDistance(latPoints, content[2], totalDistance);
+            setTemperatureData(content);
+        }
+    }
+
+    private void setTemperatureData(String[] content) {
+        Double temperature = getConvertedTemperature(new Double(content[3]), content[4]);
+        if (resultsTemperature.get("minTemperature") == null ||
+                temperature < resultsTemperature.get("minTemperature")) {
+            resultsTemperature.put("minTemperature", temperature);
+        }
+        if (resultsTemperature.get("maxTemperature") == null ||
+                temperature > resultsTemperature.get("maxTemperature")) {
+            resultsTemperature.put("maxTemperature", temperature);
+        }
+        resultsTemperature.put("totalTemperature",
+                resultsTemperature.get("totalTemperature") == null ? temperature :
+                        resultsTemperature.get("totalTemperature") + temperature);
     }
 
     private Double getConvertedTemperature(Double temperature, String observatory) {
@@ -71,7 +73,8 @@ public class generateFlightStatisticsService {
         return temperature;
     }
 
-    private Double calculateTotalDistance(String lastPoints, String currentPoints, Double totalDistance) {
+    private void calculateTotalDistance(StringBuilder lastPoints, String currentPoints, AtomicDouble totalDistance) {
+        setLastPoints(lastPoints, currentPoints);
         String[] lastPointsSplited = lastPoints.split(",");
         Double x1 = new Double(lastPointsSplited[0]);
         Double x2 = new Double(lastPointsSplited[1]);
@@ -79,8 +82,16 @@ public class generateFlightStatisticsService {
         String[] currentPointsSplited = currentPoints.split(",");
         Double y1 = new Double(currentPointsSplited[0]);
         Double y2 = new Double(currentPointsSplited[1]);
-
         return totalDistance + Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
+
+    private void setLastPoints(StringBuilder lastPoints, String currentPoints) {
+        clearStringBuilder(lastPoints);
+        lastPoints.append(currentPoints)
+    }
+
+    private void clearStringBuilder(StringBuilder stringBUilder) {
+        stringBUilder.setLength(0);
     }
 
     private List<String> getFileData() throws IOException {
